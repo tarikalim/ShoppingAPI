04b7c72e67a43884e4225d9516827bd2e9def755
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-09-21 10:54:36 +0300

    dockerfile and docker compose added. I also change directory structure a little bit for GitHub. requirements.txt updated.

--- ShoppingAPI/config.py
+++ ShoppingAPI/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("JAWSDB_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = os.getenv("MAIL_PORT")
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
